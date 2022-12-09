9303c3658cb5caacbd278375846d5f7e7d0351f6
Aaron Blohowiak <ablohowiak@netflix.com>
2022-12-08 23:02:50 -0800

    a little prettying

--- seven/src/main.rs
+++ seven/src/main.rs
@@ -3,11 +3,214 @@ use std::collections::HashMap;
 use std::fs;
 
 /*
-    Command(cmd name [args])
-    Dir (name, &parent)
-    File (size name)
+    Our Domain has two data types: a Dir and File.
+    Directories can contain directories and files.
+
+    Our Directories know how to compute their size and a couple weird AoC things.
 */
 
+#[derive(Debug, PartialEq)]
+
+struct File {
+    size: usize,
+    name: String,
+}
+
+#[derive(Debug, PartialEq)]
+struct Dir {
+    name: String,
+    files: Vec<File>,
+    children: HashMap<String, Dir>,
+}
+
+impl Dir {
+    fn new(s: &str) -> Dir {
+        return Dir {
+            name: s.to_string(),
+            files: vec![],
+            children: HashMap::new(),
+        };
+    }
+
+    //size of all my files and that of all my children.
+    fn size(&self) -> usize {
+        let mut total = 0;
+        for f in &self.files {
+            total += f.size;
+        }
+
+        for (_, d) in &self.children {
+            total += d.size();
+        }
+
+        return total;
+    }
+
+    //Walk the dir try to find the sum of directories that have a transitive sum less than 100k
+    // return current branch disk size and current tally
+    fn aoc_dir_sum(&self) -> (usize, usize) {
+        let mut my_size = 0;
+        let mut dir_sum = 0;
+
+        for f in &self.files {
+            my_size += f.size;
+        }
+
+        for (_, d) in &self.children {
+            let (kid_size, kid_sum) = d.aoc_dir_sum();
+            my_size += kid_size;
+            dir_sum += kid_sum;
+        }
+
+        if my_size <= 100000 {
+            dir_sum += my_size;
+        }
+        return (my_size, dir_sum);
+    }
+
+    //Walk the dir try to find the smallest directory that, when deleted, will free min_size space.
+    // return current directory size and current winning size or 0 if none found yet.
+    fn aoc_dir_size_min_above(&self, min_size: usize) -> (usize, usize) {
+        let mut my_size = 0;
+        let mut min_acceptable_so_far = 0;
+
+        for f in &self.files {
+            my_size += f.size;
+        }
+
+        for (_, d) in &self.children {
+            let (kid_size, kid_min_dirsize) = d.aoc_dir_size_min_above(min_size);
+            my_size += kid_size;
+
+            if kid_min_dirsize > 0
+                && (min_acceptable_so_far == 0 || kid_min_dirsize < min_acceptable_so_far)
+            {
+                min_acceptable_so_far = kid_min_dirsize;
+            }
+        }
+
+        if my_size > min_size && (min_acceptable_so_far == 0 || my_size < min_acceptable_so_far) {
+            min_acceptable_so_far = my_size;
+        }
+
+        return (my_size, min_acceptable_so_far);
+    }
+}
+
+use history::*;
+
+/*
+the challenge that i struggled with was taking a vector of parsed command history lines
+    and turning this into a tree.
+I probably should have actually read about ownership and lifetimes before doing AoC in rust :D
+One of the challenges here is that not only do i want to build it recursively,
+    but I want to consume the stream of tokens in an inner invocation and then resume from
+    where i left off on the outer invocation. This forced me into a mutable mutable ref to an
+    iterator so i could lend it down the stack and recieve it back in order to continue.
+not sure if i could get away with eliding some of the hints, but this makes compiler happy :/
+
+*/
+fn build<'h>(
+    mut hist: &'h mut Iter<'h, history::Line<'h>>,
+    cwd: &mut Dir,
+) -> &'h mut Iter<'h, history::Line<'h>> {
+    while let Some(line) = hist.next() {
+        match *line {
+            Line::Dir { name } => {
+                cwd.children.insert(name.to_string(), Dir::new(name));
+            }
+
+            Line::File { name, size } => {
+                let file = File {
+                    name: name.to_string(),
+                    size: size,
+                };
+
+                cwd.files.push(file);
+            }
+
+            //this is only done once so we cheat and ignore it.
+            Line::Command {
+                name: "cd",
+                arg: Some("/"),
+            }
+            | Line::Command {
+                name: "ls",
+                arg: None,
+            } => {}
+
+            //pop the stack
+            Line::Command {
+                name: "cd",
+                arg: Some(".."),
+            } => return hist,
+
+            Line::Command {
+                name: "cd",
+                arg: Some(name),
+            } => {
+                //get a mutable reference to the directory
+                let dir = cwd
+                    .children
+                    .get_mut(&name.to_string())
+                    .expect("trying to enter a directory that does not exist");
+
+                /* find the dir with the same name, then call build inside that dir with the remaining history.
+                when we've cd .. back to here, resume processing. */
+                hist = build(hist, dir);
+            }
+
+            Line::Command {name, arg} => {
+                panic!("Unsupported command: {:?} {:?}", name, arg);
+            }
+        }
+    }
+
+    return hist;
+}
+
+fn aoc_min_delete(root: &Dir) -> usize {
+    /* The total disk space available to the filesystem is 70000000.
+    To run the update, you need unused space of at least 30000000.
+    You need to find a directory you can delete that will free up enough space to run the update.
+
+    Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update.
+    What is the total size of that directory? */
+
+    let du = root.size();
+
+    let current_free = 70000000 - du;
+    let min_free = 30000000;
+
+    let additional_to_delete = min_free - current_free;
+
+    let (_, output) = root.aoc_dir_size_min_above(additional_to_delete);
+
+    return output;
+}
+
+fn main() {
+    let input = fs::read_to_string("./7.input").expect("Error while reading");
+    let result = history::parse_input(&input);
+
+    let mut root = Dir {
+        name: "/".to_string(),
+        files: Vec::new(),
+        children: HashMap::new(),
+    };
+
+    let h = result.unwrap().1;
+    let mut hist = h.iter();
+    build(&mut hist, &mut root);
+
+    let (_, output) = root.aoc_dir_sum();
+    println!("{:?}", output);
+
+    let output = aoc_min_delete(&root);
+    println!("{:?}", output);
+}
+
+/* represents the file input. */
 mod history {
 
     use nom::{
@@ -25,8 +228,6 @@ mod history {
         File { size: usize, name: &'a str },
         Dir { name: &'a str },
     }
-
-    //three options: command line, dir name or file size
 
     fn parse_command(i: &str) -> IResult<&str, Line> {
         let name = preceded(tag("$ "), is_not(" \n"));
@@ -106,90 +307,6 @@ mod history {
             let a = "$ cd /\n$ ls\ndir a\n";
             println!("{:?}", a);
         }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-
-struct File {
-    size: usize,
-    name: String,
-}
-
-#[derive(Debug, PartialEq)]
-struct Dir {
-    name: String,
-    files: Vec<File>,
-    children: HashMap<String, Dir>,
-}
-
-impl Dir {
-    fn new(s: &str) -> Dir {
-        return Dir {
-            name: s.to_string(),
-            files: vec![],
-            children: HashMap::new(),
-        };
-    }
-
-    //size of all my files and that of all my children.
-    fn size(&self) -> usize {
-        let mut total = 0;
-        for f in &self.files {
-            total += f.size;
-        }
-
-        for (_, d) in &self.children {
-            total += d.size();
-        }
-
-        return total;
-    }
-
-    fn aoc_dir_sum(&self) -> (usize, usize) {
-        let mut my_size = 0;
-        let mut dir_sum = 0;
-
-        for f in &self.files {
-            my_size += f.size;
-        }
-
-        for (_, d) in &self.children {
-            let (kid_size, kid_sum) = d.aoc_dir_sum();
-            my_size += kid_size;
-            dir_sum += kid_sum;
-        }
-
-        if my_size <= 100000 {
-            dir_sum += my_size;
-        }
-        return (my_size, dir_sum);
-    }
-
-    fn aoc_dir_size_min_above(&self, min_size: usize) -> (usize, usize) {
-        let mut my_size = 0;
-        let mut min_acceptable_so_far = 0;
-
-        for f in &self.files {
-            my_size += f.size;
-        }
-
-        for (_, d) in &self.children {
-            let (kid_size, kid_min_dirsize) = d.aoc_dir_size_min_above(min_size);
-            my_size += kid_size;
-
-            if kid_min_dirsize > 0
-                && (min_acceptable_so_far == 0 || kid_min_dirsize < min_acceptable_so_far)
-            {
-                min_acceptable_so_far = kid_min_dirsize;
-            }
-        }
-
-        if my_size > min_size && (min_acceptable_so_far == 0 || my_size < min_acceptable_so_far) {
-            min_acceptable_so_far = my_size;
-        }
-
-        return (my_size, min_acceptable_so_far);
     }
 }
 
@@ -407,106 +524,4 @@ $ ls
         let output = aoc_min_delete(&root);
         assert_eq!(output, 24933642);
     }
-}
-
-use history::*;
-
-//not sure if i could get away with eliding some of the hints, but this makes compiler happy :/
-fn build<'h>(
-    mut hist: &'h mut Iter<'h, history::Line<'h>>,
-    cwd: &mut Dir,
-) -> &'h mut Iter<'h, history::Line<'h>> {
-    while let Some(line) = hist.next() {
-        match *line {
-            Line::Dir { name } => {
-                cwd.children.insert(name.to_string(), Dir::new(name));
-            }
-
-            Line::File { name, size } => {
-                let file = File {
-                    name: name.to_string(),
-                    size: size,
-                };
-
-                cwd.files.push(file);
-            }
-
-            //this is only done once so we cheat and ignore it.
-            Line::Command {
-                name: "cd",
-                arg: Some("/"),
-            } => {}
-
-            Line::Command {
-                name: "ls",
-                arg: None,
-            } => {}
-
-            Line::Command {
-                name: "cd",
-                arg: Some(".."),
-            } => return hist,
-
-            Line::Command {
-                name: "cd",
-                arg: Some(name),
-            } => {
-                //get a mutable reference to the directory
-                let dir = cwd
-                    .children
-                    .get_mut(&name.to_string())
-                    .expect("trying to enter a directory that does not exist");
-                /* find the dir with the same name, then call build inside that dir with the remaining history */
-                hist = build(hist, dir);
-            }
-
-            _ => {
-                println!("Not supported {:?}", line);
-                return hist;
-            }
-        }
-    }
-
-    return hist;
-}
-
-fn aoc_min_delete(root: &Dir) -> usize {
-    /* The total disk space available to the filesystem is 70000000.
-    To run the update, you need unused space of at least 30000000.
-    You need to find a directory you can delete that will free up enough space to run the update.
-
-    Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update.
-    What is the total size of that directory? */
-
-    let du = root.size();
-
-    let current_free = 70000000 - du;
-    let min_free = 30000000;
-
-    let additional_to_delete = min_free - current_free;
-
-    let (_, output) = root.aoc_dir_size_min_above(additional_to_delete);
-
-    return output;
-}
-
-fn main() {
-    let input = fs::read_to_string("./7.input").expect("Error while reading");
-    let result = history::parse_input(&input);
-
-    let mut root = Dir {
-        name: "/".to_string(),
-        files: Vec::new(),
-        children: HashMap::new(),
-    };
-
-    let h = result.unwrap().1;
-    let mut hist = h.iter();
-    build(&mut hist, &mut root);
-
-    let (_, output) = root.aoc_dir_sum();
-    println!("{:?}", output);
-
-    let output = aoc_min_delete(&root);
-    println!("{:?}", output);
 }
