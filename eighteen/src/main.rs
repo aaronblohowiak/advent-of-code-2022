f4c57e03c320453f1fb143dcf727b30a444c141e
Aaron P Blohowiak <aaron.blohowiak@gmail.com>
2022-12-18 00:45:26 -0800

    part 1 and 2. easy day.

---
+++ eighteen/src/main.rs
@@ -0,0 +1,145 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+use std::fs;
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq, Default)]
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Point3d {
+    fn from(i: (i32, i32, i32)) -> Point3d {
+        Point3d {
+            x: i.0,
+            y: i.1,
+            z: i.2,
+        }
+    }
+}
+
+impl<'a, 'b> Add<&'b Point3d> for &'a Point3d {
+    type Output = Point3d;
+
+    fn add(self, other: &'b Point3d) -> Point3d {
+        Point3d {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+const UP: Point3d = Point3d { x: 0, y: 0, z: 1 };
+const DOWN: Point3d = Point3d { x: 0, y: 0, z: -1 };
+
+const RIGHT: Point3d = Point3d { x: 1, y: 0, z: 0 };
+const LEFT: Point3d = Point3d { x: -1, y: 0, z: 0 };
+
+const BACK: Point3d = Point3d { x: 0, y: 1, z: 0 };
+const FRONT: Point3d = Point3d { x: 0, y: -1, z: 0 };
+
+const DIRECTIONS: [Point3d; 6] = [UP, DOWN, LEFT, RIGHT, BACK, FRONT];
+
+fn main() {
+    let points = parse_input("18.input");
+
+    //i could construct a tree with six edges per node and that would make this fast. OR, brute force. Hulk smash!
+
+    println!("Part 1: {}", part1(&points));
+
+    println!("Part 2: {}", part2(&points));
+}
+
+fn part1(points: &HashSet<Point3d>) -> i32 {
+    points
+        .iter()
+        .map(|p| {
+            DIRECTIONS
+                .iter()
+                .map(|d| !points.contains(&(p + d)) as i32)
+                .sum::<i32>()
+        })
+        .sum()
+}
+
+//make a box that fully sorrounds the lava with steam, then for every lava point, count directions that touch steam.
+fn part2(lava_points: &HashSet<Point3d>) -> i32 {
+    let mut maxes = Point3d::default();
+    let mut mins = Point3d::default();
+
+    lava_points.iter().for_each(|p| {
+        maxes.x = maxes.x.max(p.x);
+        maxes.y = maxes.y.max(p.y);
+        maxes.z = maxes.z.max(p.z);
+
+        mins.x = mins.x.min(p.x);
+        mins.y = mins.y.min(p.y);
+        mins.z = mins.z.min(p.z);
+    });
+
+    maxes = &maxes + &Point3d{x:1, y:1, z:1};
+    mins = &mins + &Point3d{x:-1, y:-1, z:-1};
+
+    let mut steam_box: HashSet<Point3d> = HashSet::default();
+
+    fn steam_fill(
+        steam_box: &mut HashSet<Point3d>,
+        lava_points: &HashSet<Point3d>,
+        from: &Point3d,
+        maxes: &Point3d,
+        mins: &Point3d,
+    ) {
+        steam_box.insert(*from);
+
+        for dir in DIRECTIONS {
+            let next = from + &dir;
+
+            if contained3d(&next, maxes, mins)
+                && !lava_points.contains(&next)
+                && !steam_box.contains(&next)
+            {
+                steam_fill(steam_box, lava_points, &next, maxes, mins);
+            }
+        }
+    }
+
+    steam_fill(&mut steam_box, lava_points, &maxes, &maxes, &mins);
+
+    lava_points
+        .iter()
+        .map(|p| {
+            DIRECTIONS
+                .iter()
+                .map(|d| {
+                    let n = &(p + d);
+                    steam_box.contains(n) as i32
+                })
+                .sum::<i32>()
+        })
+        .sum()
+}
+
+fn contained3d(next: &Point3d, maxes: &Point3d, mins: &Point3d) -> bool {
+    next.x >= mins.x
+        && next.x <= maxes.x
+        && next.y >= mins.y
+        && next.y <= maxes.y
+        && next.z >= mins.z
+        && next.z <= maxes.z
+}
+
+fn parse_input(fname: &str) -> HashSet<Point3d> {
+    let input = fs::read_to_string(fname).expect("could not read file");
+    input
+        .lines()
+        .flat_map(|l| {
+            l.split(',')
+                .map(|s| s.parse().unwrap())
+                .tuples()
+                .map(Point3d::from)
+        })
+        .collect()
+}
